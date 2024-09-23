0746998fa8d799c10297fed2ff227d46cddd4dfd
octavio <octaluponio@gmail.com>
2024-09-23 19:18:19 -0300

    Agregar archivos del proyecto

---
+++ luponio_madereras/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using luponio_madereras.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace luponio_madereras.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ luponio_madereras/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace luponio_madereras.Models
+{
+    public class Producto
+    {
+        public int idproducto { get; set; }
+        public string nombre { get; set; }
+        public int stock { get; set; }
+        public int idcategoria { get; set; }
+        public int idproveedor { get; set; }
+    }
+}
